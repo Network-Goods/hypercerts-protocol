a1d18f2b33002cbf8448d2232615215c284c2819
bitbeckers <code@bitbeckers.com>
2022-08-31 14:49:12 +0200

    fix: typo

--- test/wellKnown.ts
+++ test/wellKnown.ts
@@ -2,7 +2,7 @@ import { ethers } from "hardhat";
 
 import { toHashMap } from "./utils";
 
-export const HypercertMinter_V0 = "HypercertMinterv0";
+export const HypercertMinter_V0 = "HypercertMinterV0";
 export const HypercertMinter_Upgrade = "HypercertMinterUpgrade";
 
 export const UPGRADER_ROLE = ethers.utils.id("UPGRADER_ROLE");
