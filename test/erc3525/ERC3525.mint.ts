76fd540b65c679ad7f161e6df7384823f47f9967
bitbeckers <code@bitbeckers.com>
2022-09-27 13:56:43 +0200

    fix(test): updated assertion calls for overrides

--- test/erc3525/ERC3525.mint.ts
+++ test/erc3525/ERC3525.mint.ts
@@ -1,4 +1,5 @@
 import { expect } from "chai";
+import { BigNumber } from "ethers";
 import { ethers } from "hardhat";
 
 import { setupTestERC3525 } from "../setup";
@@ -8,7 +9,7 @@ export function shouldBehaveLikeSemiFungibleTokenMint(): void {
     it("allows for minting a single token in a slot with a given value", async function () {
       const { sft, user } = await setupTestERC3525();
 
-      await expect(sft.balanceOf(0)).to.be.revertedWith("ERC3525: balance query for nonexistent token");
+      await expect(sft["balanceOf(uint256)"](0)).to.be.revertedWith("ERC3525: balance query for nonexistent token");
       await expect(sft.slotOf(0)).to.be.revertedWith("ERC3525: slot query for nonexistent token");
       await expect(sft.mintValue(ethers.constants.AddressZero, 0, 0, 1_000_000)).to.be.revertedWith(
         "ERC3525: mint to the zero address",
@@ -22,10 +23,9 @@ export function shouldBehaveLikeSemiFungibleTokenMint(): void {
         .withArgs(0, 1, 1_000_000);
       await expect(sft.mintValue(user.address, 1, 0, 1_000_000)).to.be.revertedWith("ERC3525: token already minted");
 
-      expect(sft.slotCount(1));
-      expect(sft.tokenSupplyInSlot(0)).to.be("1");
-      expect(sft.tokenInSlotByIndex(0, 0).to.be("1"));
-      expect(sft.balanceOf(1)).to.be("1_000_000");
+      expect(await sft["tokenSupplyInSlot"](0)).to.be.eq("1");
+      expect(await sft["tokenInSlotByIndex"](0, 0).to.be.eq("1"));
+      expect(await sft["balanceOf(uint256)"](1)).to.be.eq("1000000");
     });
 
     it("allows for minting another token in a slot with a given value", async function () {
@@ -35,19 +35,19 @@ export function shouldBehaveLikeSemiFungibleTokenMint(): void {
         .to.emit(sft, "TransferValue")
         .withArgs(0, 1, 1_000_000);
 
-      expect(sft.balanceOf(1)).to.be("1_000_000");
-      await expect(sft.balanceOf(2)).to.be.revertedWith("ERC3525: slot query for nonexistent token");
+      expect(await sft["balanceOf(uint256)"](1)).to.be.eq("1000000");
+      await expect(sft["balanceOf(uint256)"](2)).to.be.revertedWith("ERC3525: balance query for nonexistent token");
 
       await expect(sft.mintValue(user.address, 2, 0, 2_000_000))
         .to.emit(sft, "TransferValue")
         .withArgs(0, 2, 2_000_000);
 
-      expect(sft.tokenSupplyInSlot(0)).to.be("2");
-      expect(sft.tokenInSlotByIndex(0, 0).to.be("1"));
-      expect(sft.tokenInSlotByIndex(0, 1).to.be("2"));
+      expect(await sft["tokenSupplyInSlot"](0)).to.be.eq("2");
+      expect(await sft["tokenInSlotByIndex"](0, 0).to.be.eq("1"));
+      expect(await sft["tokenInSlotByIndex"](0, 1).to.be.eq("2"));
 
-      expect(sft.balanceOf(1)).to.be("1_000_000");
-      expect(sft.balanceOf(2)).to.be("2_000_000");
+      expect(await sft["balanceOf(uint256)"](1)).to.be.eq("1000000");
+      expect(await sft["balanceOf(uint256)"](2)).to.be.eq("2000000");
     });
   });
 }
--- test/erc3525/ERC3525.transfer.ts
+++ test/erc3525/ERC3525.transfer.ts
@@ -7,28 +7,20 @@ export function shouldBehaveLikeSemiFungibleTokenTransfer(): void {
     it("allows for transfering values between tokens with identical slots", async function () {
       const { sft, user } = await setupTestERC3525();
 
-      await expect(sft.transferValue(user.address, 1, 2, 1_234_5678)).to.be.revertedWith(
-        "ERC35255: transfer from nonexistent token",
-      );
+      await expect(sft.transfer(1, 2, 1_234_5678)).to.be.revertedWith("ERC35255: transfer from nonexistent token");
 
       await sft.mintValue(user.address, 1, 0, 1_000_000);
 
-      await expect(sft.transferValue(user.address, 1, 2, 500_000)).to.be.revertedWith(
-        "ERC35255: transfer to nonexistent token",
-      );
+      await expect(sft.transfer(1, 2, 500_000)).to.be.revertedWith("ERC35255: transfer to nonexistent token");
 
       await sft.mintValue(user.address, 2, 0, 2_000_000);
 
-      await expect(sft.transferValue(user.address, 1, 2, 8_796_543)).to.be.revertedWith(
-        "ERC3525: transfer amount exceeds balance",
-      );
+      await expect(sft.transfer(1, 2, 8_796_543)).to.be.revertedWith("ERC3525: transfer amount exceeds balance");
 
-      await expect(sft.transferValue(user.address, 1, 2, 500_000))
-        .to.emit(sft, "TransferValue")
-        .withArgs(1, 2, 500_000);
+      await expect(sft.transfer(1, 2, 500_000)).to.emit(sft, "TransferValue").withArgs(1, 2, 500_000);
 
-      expect(sft.balanceOf(1)).to.be("500_000");
-      expect(sft.balanceOf(2)).to.be("2_500_000");
+      expect(await sft["balanceOf(uint256)"](1)).to.be.eq("500000");
+      expect(await sft["balanceOf(uint256)"](2)).to.be.eq("2500000");
     });
 
     it("does not allow for transfering values between tokens with different slots", async function () {
@@ -36,12 +28,10 @@ export function shouldBehaveLikeSemiFungibleTokenTransfer(): void {
       await sft.mintValue(user.address, 1, 0, 1_000_000);
       await sft.mintValue(user.address, 2, 1, 2_000_000);
 
-      await expect(sft.transferValue(user.address, 1, 2, 500_000)).to.be.revertedWith(
-        "ERC3535: transfer to token with different slot",
-      );
+      await expect(sft.transfer(1, 2, 500_000)).to.be.revertedWith("ERC3535: transfer to token with different slot");
 
-      expect(sft.balanceOf(1)).to.be("1_000_000");
-      expect(sft.balanceOf(2)).to.be("2_000_000");
+      expect(await sft["balanceOf(uint256)"](1)).to.be.eq("1000000");
+      expect(await sft["balanceOf(uint256)"](2)).to.be.eq("2000000");
     });
   });
 }
