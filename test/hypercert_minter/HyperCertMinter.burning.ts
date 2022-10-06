f12e08ddb9d2aa66e5cd09cd8ba7c952a3d6302d
Matthew O'Connell <totallyterrific@tutanota.com>
2022-10-06 18:59:30 +0200

    refactor: burn tests

--- test/hypercert_minter/HyperCertMinter.burning.ts
+++ test/hypercert_minter/HyperCertMinter.burning.ts
@@ -34,19 +34,19 @@ export function shouldBehaveLikeHypercertMinterBurning(): void {
     await expect(minter["balanceOf(uint256)"](tokenId)).to.be.revertedWith("NonExistentToken");
     await expect(minter.ownerOf(tokenId)).to.be.revertedWith("ERC721: invalid token ID");
     await expect(minter.slotOf(tokenId)).to.be.revertedWith("NonExistentToken");
-    // expect(await minter.tokenSupplyInSlot(slot)).to.be.eq(0);
+    expect(await minter.tokenSupplyInSlot(slot)).to.be.eq(0); // <--- this is still 1
     await expect(minter.tokenURI(tokenId)).to.be.revertedWith("NonExistentToken");
-    // await expect(minter.slotURI(slot)).to.be.revertedWith("NonExistentToken");
-    validateMetadata(await minter.slotURI(slot), claim);
+    await expect(minter.slotURI(slot)).to.be.revertedWith("TBC"); // <--- ?? claim still exists
   });
 
   it("prevents burning when the creator doesn't own the full slot", async function () {
     const { deployer, minter, user } = await setupTest();
     const claim = await newClaim({ fractions: [50, 50] });
+    const slot = await getClaimSlotID(claim);
     const data = encodeClaim(claim);
 
     await expect(deployer.minter.mint(deployer.address, data)).to.emit(minter, "ImpactClaimed");
-    expect(await minter.tokenSupplyInSlot(1)).to.be.eq(2);
+    expect(await minter.tokenSupplyInSlot(slot)).to.be.eq(2);
     expect(await minter["balanceOf(address)"](deployer.address)).to.equal(2);
 
     await expect(deployer.minter["transferFrom(address,address,uint256)"](deployer.address, user.address, 1)).to.emit(
@@ -54,13 +54,13 @@ export function shouldBehaveLikeHypercertMinterBurning(): void {
       "Transfer",
     );
 
-    expect(await minter.tokenSupplyInSlot(1)).to.be.eq(2);
+    expect(await minter.tokenSupplyInSlot(slot)).to.be.eq(2);
     expect(await minter["balanceOf(address)"](deployer.address)).to.equal(1);
     expect(await minter["balanceOf(address)"](user.address)).to.equal(1);
 
     await expect(deployer.minter.burn(1)).to.be.revertedWith("ERC721: caller is not token owner nor approved");
 
-    expect(await minter.tokenSupplyInSlot(1)).to.be.eq(2);
+    expect(await minter.tokenSupplyInSlot(slot)).to.be.eq(2);
     expect(await minter["balanceOf(address)"](deployer.address)).to.equal(1);
     expect(await minter["balanceOf(address)"](user.address)).to.equal(1);
   });
@@ -68,18 +68,21 @@ export function shouldBehaveLikeHypercertMinterBurning(): void {
   it("prevents burning when the owner isn't the creator", async function () {
     const { deployer, minter, anon } = await setupTest();
     const claim = await newClaim({ fractions: [50, 50] });
+    const slot = await getClaimSlotID(claim);
     const data = encodeClaim(claim);
 
     await expect(deployer.minter.mint(anon.address, data)).to.emit(minter, "ImpactClaimed");
-    expect(await minter.tokenSupplyInSlot(1)).to.be.eq(2);
+    expect(await minter.tokenSupplyInSlot(slot)).to.be.eq(2);
     expect(await minter["balanceOf(address)"](deployer.address)).to.equal(0);
+    expect(await minter["balanceOf(address)"](minter.address)).to.equal(0);
     expect(await minter["balanceOf(address)"](anon.address)).to.equal(2);
 
-    await expect(anon.minter.burn(1)).to.be.revertedWith("HyperCert: only creator can burn claim");
+    await expect(anon.minter.burn(1)).to.be.revertedWith("HyperCert: only creator can burn claim"); // not reverted
     await expect(deployer.minter.burn(1)).to.be.revertedWith("HyperCert: owner isn't creator");
 
-    expect(await minter.tokenSupplyInSlot(1)).to.be.eq(2);
+    expect(await minter.tokenSupplyInSlot(slot)).to.be.eq(2);
     expect(await minter["balanceOf(address)"](deployer.address)).to.equal(0);
+    expect(await minter["balanceOf(address)"](minter.address)).to.equal(0);
     expect(await minter["balanceOf(address)"](anon.address)).to.equal(2);
   });
 }
