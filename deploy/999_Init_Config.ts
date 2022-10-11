41f80300196de17dc21312669575dd40ef697ebf
bitbeckers <code@bitbeckers.com>
2022-10-11 18:55:35 +0200

    fix(deploy): fresh goerli deploy with bug fix

--- deploy/999_Init_Config.ts
+++ deploy/999_Init_Config.ts
@@ -11,15 +11,15 @@ const deploy: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 
   const colorsSize = await read("HyperCertSVG", { from: deployer }, "colorsCounter");
 
-  if (colorsSize == 0) {
-    await execute("HyperCertSVG", { from: deployer, log: true }, "addColors",["#F3556F", "#121933", "#D4BFFF"]); //
-     await execute("HyperCertSVG", { from: deployer, log: true }, "addColors",["#FFBFCA", "#FFFFFF", "#5500FF"]); //
-     await execute("HyperCertSVG", { from: deployer, log: true }, "addColors",["#25316D", "#121933", "#80E5D3"]); //
-     await execute("HyperCertSVG", { from: deployer, log: true }, "addColors",["#25316D", "#FFFFFF", "#F3556F"]); //
-     await execute("HyperCertSVG", { from: deployer, log: true }, "addColors",["#80E5D3", "#FFFFFF", "#121933"]); //
-     await execute("HyperCertSVG", { from: deployer, log: true }, "addColors",["#FEF5AC", "#FFFFFF", "#25316D"]); //
-     await execute("HyperCertSVG", { from: deployer, log: true }, "addColors",["#F3556F", "#121933", "#FFBFCA"]); //
-     await execute("HyperCertSVG", { from: deployer, log: true }, "addColors",["#5500FF", "#121933", "#FFCC00"]); //
+  if (colorsSize == 1) {
+    await execute("HyperCertSVG", { from: deployer, log: true }, "addColors", ["#F3556F", "#121933", "#D4BFFF"]); //
+    await execute("HyperCertSVG", { from: deployer, log: true }, "addColors", ["#FFBFCA", "#FFFFFF", "#5500FF"]); //
+    await execute("HyperCertSVG", { from: deployer, log: true }, "addColors", ["#25316D", "#121933", "#80E5D3"]); //
+    await execute("HyperCertSVG", { from: deployer, log: true }, "addColors", ["#25316D", "#FFFFFF", "#F3556F"]); //
+    await execute("HyperCertSVG", { from: deployer, log: true }, "addColors", ["#80E5D3", "#FFFFFF", "#121933"]); //
+    await execute("HyperCertSVG", { from: deployer, log: true }, "addColors", ["#FEF5AC", "#FFFFFF", "#25316D"]); //
+    await execute("HyperCertSVG", { from: deployer, log: true }, "addColors", ["#F3556F", "#121933", "#FFBFCA"]); //
+    await execute("HyperCertSVG", { from: deployer, log: true }, "addColors", ["#5500FF", "#121933", "#FFCC00"]); //
   }
 
   const currentBackground = await read("HyperCertSVG", { from: deployer }, "backgrounds", 0);
