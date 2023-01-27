b94baec449f089a447ee4dea674ce0f429583659
bitbeckers <code@bitbeckers.com>
2023-01-27 16:44:17 +0100

    fix(lint): classic

--- test/foundry/SemiFungible1155.burning.t.sol
+++ test/foundry/SemiFungible1155.burning.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity >=0.8.4;
+pragma solidity >=0.8.16;
 
 import { console2 } from "forge-std/console2.sol";
 import { PRBTest } from "prb-test/PRBTest.sol";
